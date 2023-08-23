bb86b045da7baa645a1b4c71b44af961df8b502d
lily <delanylily@gmail.com>
2023-08-23 16:58:35 +0200

    Fixed imports in spec files

--- src/app/user-profile/components/search-book/search-book.component.spec.ts
+++ src/app/user-profile/components/search-book/search-book.component.spec.ts
@@ -1,9 +1,7 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { AddBookComponent } from '../add-book/add-book.component';
 
-import { AddBookComponent } from './search-book.component';
 
 describe('AddBookComponent', () => {
   let component: AddBookComponent;
